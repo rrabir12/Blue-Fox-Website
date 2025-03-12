c85b1bc646636a1e947408f14b1c21b7aa448327
Rijan Shrestha <rijan0518@gmail.com>
2025-03-12 23:18:25 +0545

    services added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Homepage from './components/Homepage';
 import About from './pages/About/about';  // Import About component
 import Footer from './components/Footer';
+import AiEnable from './pages/ai-enable/AiEnable';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />  {/* Fix: Define About route */}
+        <Route path="/about" element={<About />} /> {/* Fix: Define About route */}
+        <Route path='/ai_enable' element={<AiEnable />} />
       </Routes>
       <Footer />
     </div>
